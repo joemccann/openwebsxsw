d29bdcc1a91227ca81e6bd9a11a31a418e9b71db
Joe McCann <joseph.isaac@gmail.com>
2013-03-08 12:46:47 -0600

    re-added registration; initial commit of email template

--- public/js/boot.prod.js
+++ public/js/boot.prod.js
@@ -2,7 +2,7 @@
 var $lib = ('__proto__' in {} ? 'zepto.min' : 'jquery.min')
 require(["log",  $lib ], function(l, zepto){
   log($lib + ' and Log loaded...')
-  require(["openwebsxsw-0.0.4-7.min"], function(openwebsxsw){
+  require(["openwebsxsw-0.0.4-8.min"], function(openwebsxsw){
     log('All JS files loaded...')
   })
 })
